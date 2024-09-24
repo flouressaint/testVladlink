2c531c0b2fd5a099aca21ef334d559b4c4cfa463
flouressaint <kadomtcev.va@students.dvfu.ru>
2024-09-25 01:49:19 +1000

    list_menu.php page

---
+++ list_menu.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>list menu</title>
+</head>
+
+<body>
+    <?php
+    $env = parse_ini_file('./.env');
+
+    $db = new PDO(
+        sprintf(
+            'pgsql:host=localhost;port=%s;dbname=%s;user=%s;password=%s',
+            $env['POSTGRES_PORT'],
+            $env['POSTGRES_DB'],
+            $env['POSTGRES_USER'],
+            $env['POSTGRES_PASSWORD']
+        )
+    );
+
+
+    $statement = $db->prepare("SELECT * FROM categories");
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    function write(array $elements, $parentId = 0, $level = "")
+    {
+        foreach ($elements as $element) {
+            if ($element['parent_id'] == $parentId) {
+                echo  $level . $element['name'] . "<br>";
+                $children = write(
+                    $elements,
+                    $element['id'],
+                    $level . "&nbsp;&nbsp;",
+                );
+            }
+        }
+    }
+
+    write($result);
+    ?>
+</body>
+
+
+
+</html>
