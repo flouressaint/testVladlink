fe85c19813330dae9115f01ba0da8462b0034504
flouressaint <kadomtcev.va@students.dvfu.ru>
2024-09-25 00:32:01 +1000

    export in file with url

---
+++ export.php
@@ -0,0 +1,52 @@
+<?php
+
+class Export
+{
+    private $file;
+    public function __construct($filename)
+    {
+        $this->file = fopen($filename, "w") or die("Unable to open file!");
+    }
+
+    public function __destruct()
+    {
+        fclose($this->file);
+    }
+
+    public function write(array $elements, $parentId = 0, $level = "", $url = "/")
+    {
+        foreach ($elements as $element) {
+            if ($element['parent_id'] == $parentId) {
+                fwrite($this->file, $level . $element['name'] . "  " . $url . $element['alias'] . "\n");
+                $children = $this->write(
+                    $elements,
+                    $element['id'],
+                    $level . "  ",
+                    $url . $element['alias'] . "/"
+                );
+            }
+        }
+    }
+
+}
+
+
+$env = parse_ini_file('.env');
+
+$db = new PDO(
+    sprintf(
+        'pgsql:host=localhost;port=%s;dbname=%s;user=%s;password=%s',
+        $env['POSTGRES_PORT'],
+        $env['POSTGRES_DB'],
+        $env['POSTGRES_USER'],
+        $env['POSTGRES_PASSWORD']
+    )
+);
+
+
+$statement = $db->prepare("SELECT * FROM categories");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$export = new Export('type_a.txt');
+$export->write($result);
--- import.php
+++ import.php
@@ -32,10 +32,10 @@ $db = new PDO(
         $env['POSTGRES_PASSWORD']
     )
 );
-$statement = $db->prepare("SELECT * FROM categories");
-$statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($result);
+// $statement = $db->prepare("SELECT * FROM categories");
+// $statement->execute();
+// $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+// echo json_encode($result);
 
 
 $json = file_get_contents('categories.json');
@@ -56,7 +56,12 @@ $table = array_values_recursive($json_data);
 // print_r($table);
 
 foreach ($table as $value) {
-    $statement = $db->prepare("INSERT INTO categories (id, name, alias, parent_id) VALUES (:id, :name, :alias, :parent_id)");
+    $statement = $db->prepare("
+    INSERT INTO categories (id, name, alias, parent_id) 
+        VALUES (:id, :name, :alias, :parent_id) 
+    ON CONFLICT (id) DO UPDATE
+        SET name = :name, alias = :alias, parent_id = :parent_id
+    ");
     $statement->bindValue(':id', $value->id);
     $statement->bindValue(':name', $value->name);
     $statement->bindValue(':alias', $value->alias);
